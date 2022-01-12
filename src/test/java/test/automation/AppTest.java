f2d5eb3c480852386e581cf5dfdef1091e600640
gauravS <gauravshukla01@gmail.com>
2022-01-13 00:39:06 +0100

    base files added

---
+++ src/test/java/test/automation/AppTest.java
@@ -0,0 +1,20 @@
+package test.automation;
+
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("Hello back ");
+    }
+}
