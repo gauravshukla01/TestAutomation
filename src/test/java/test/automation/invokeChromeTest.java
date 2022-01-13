ce5b9e3395e68de4349d329b516c6e9edb4000f8
gauravS <gauravshukla01@gmail.com>
2022-01-13 01:05:53 +0100

    added test class for invoking chrome browser

---
+++ src/test/java/test/automation/invokeChromeTest.java
@@ -0,0 +1,15 @@
+package test.automation;
+
+import org.testng.annotations.Test;
+
+public class invokeChromeTest {
+
+
+    @Test
+    public void invokeChrome()
+    {
+
+        System.out.println("will invoke chrome browser ");
+    }
+
+}
